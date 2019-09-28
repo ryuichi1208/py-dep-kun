1ae409f3cd5b25c7433f4eb4fa343857fb1125ed
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-09-28 14:45:27 +0900

    Create bun_build.sh

---
+++ script/bun_build.sh
@@ -0,0 +1,8 @@
+# Make sure that Cake has been installed.
+if [ ! -f "$CAKE_EXE" ]; then
+    echo "Could not find Cake.exe at '$CAKE_EXE'."
+    exit 1
+fi
+
+# Start Cake
+exec mono "$CAKE_EXE" $SCRIPT "${CAKE_ARGUMENTS[@]}"
