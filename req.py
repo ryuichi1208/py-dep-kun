1049ecd807320659c6920f3db2c8003049ae8a0d
ryuichi1208 <ryucrosskey@gmail.com>
2019-10-08 23:25:11 +0900

    fix prottype

--- req.py
+++ req.py
@@ -87,5 +87,34 @@ def do_encrypto(key: str, iv=b'0*'*16):
     cipher = aes.encrypt("python script000".encode("ascii"))
     return cipher
 
-do_encrypto("aaa")
+class DefPrintMethod(object):
+    """
+     __class__
+     __delattr__
+     __dict__
+     __dir__
+     __doc__
+     __eq__
+     __format__
+     __ge__
+     __getattribute__
+     __gt__
+     __hash__
+     __init__
+     __init_subclass__
+     __le__
+     __lt__
+     __module__
+     __ne__
+     __new__
+     __reduce__
+     __reduce_ex__
+     __repr__
+     __setattr__
+     __sizeof__
+     __str__
+     __subclasshook__
+     __weakref__
+    """
+    pass
 
